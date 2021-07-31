9e7b71313c1a78318a672c3e3545f06d56f28ff6
shubh4527 <75565285+shubh4527@users.noreply.github.com>
2021-07-31 09:11:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.use(express.static('img'));
 //make way for some custom css, js and images
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/img', express.static(__dirname + '/img'));
-app.use('shubham.jpg', express.static(__dirname + '/img'));
 var server = app.listen(9005, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
